7e612cd313fa6980b690fdf8ff83e61b575f65ba
ouzhihuo <1027606960@qq.com>
2019-08-10 11:13:08 +0800

    js

--- routes/index.js
+++ routes/index.js
@@ -23,86 +23,14 @@ app.use(session({
 app.use(bodyParser());
 app.use(cors());
 
-// const compose = require('koa-compose')
-// const glob = require('glob')
-// const { resolve } = require('path')
-
 const registerRouter  = require('../req/index.js');
 
-// import reqIndx from '../req/index.js'
 // import reqProduct from '../req/product.js'
-// import reqMap from '../req/map.js'
-// import reqInfo from '../req/info.js'
-// import reqUsers from '../req/users.js'
-// import reqList from '../req/list.js'
-
-// router.get('/map', async (ctx, next) => {
-//   const data = await dbs.MapData.findOne()
-//   const result = {
-//     code:200,
-//     response: data,
-//     ts: 12345
-//   }
-//   ctx.body = result
-// })
-
-
-// router.get('/usertables', async (ctx, next) => {
-//   const data = await dbs.UserTables.find()
-//   const result = {
-//     code:200,
-//     response: data,
-//     ts: 12345
-//   }
-//   ctx.body = result
-//   // return result
-// })
-
-// router.get('/users', async (ctx, next) => {
-//   const data = await dbs.User.find()
-//   const result = {
-//     code:200,
-//     response: data,
-//     ts: 12345
-//   }
-//   ctx.body = result
-//   // return result
-// })
-
-// compose 整合多个路由文件
-// var registerRouter = function() {
-//     let routers = [];
-//     glob.sync(resolve(__dirname, '../req/', '**/*.js'))
-//         .filter(value => (value.indexOf('index.js') === -1))
-//         .map(router => {
-//             routers.push(require(router).routes())
-//             routers.push(require(router).allowedMethods())
-//         })
-//     return compose(routers)
-// }
-
-// 地图接口
-// app.use(reqMap.routes())
-// app.use(reqMap.allowedMethods())
 
 // // 产品接口
 // app.use(reqProduct.routes())
 // app.use(reqProduct.allowedMethods())
 
-// // 个人信息接口
-// app.use(reqInfo.routes())
-// app.use(reqInfo.allowedMethods())
-
-// // 用户接口
-// app.use(reqUsers.routes())
-// app.use(reqUsers.allowedMethods())
-
-// // 列表接口
-// app.use(reqList.routes())
-// app.use(reqList.allowedMethods())
-
-// app.use(reqIndx.routes())
-// app.use(reqIndx.allowedMethods())
 
 app.use(registerRouter());
 
@@ -110,8 +38,6 @@ app.use(registerRouter());
 // app.use(router.routes())  //启动路由
  
 // app.use(router.allowedMethods())  // 建议配置 可以省略  作用是设置一个对应的响应头
-
-// app.use(registerRouter());
 
 app.listen(9000)
 
